9512609f701df25f9e0f7b4cbf5f3083709c5d53
Mickiepk <115391401+Mickiepk@users.noreply.github.com>
2024-11-29 20:20:13 +0700

    registratoinForm model

---
+++ src/models/RegistrationForm.ts
@@ -0,0 +1,11 @@
+export interface StudentFormData {
+    studentId: string;
+    firstName: string;
+    lastName: string;
+    school: string;
+    age: number | null;
+    address: string;
+    homeLocation: string;
+    guardianName: string;
+    phoneNumber: string;
+  }
